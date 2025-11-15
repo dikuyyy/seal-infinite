420ec363f43906547377f3b33211d4a11f865e5f
Andhika Guntur Slamet <andhika.gs@bts.id>
2025-11-15 20:56:04 +0700

    feat: update rank table layout and improve data row structure for better readability

--- resources/views/pages/rank/index.blade.php
+++ resources/views/pages/rank/index.blade.php
@@ -29,7 +29,6 @@
                     <div class="rank-table-container">
                         <!-- Table Header -->
                         <div class="rank-table-header">
-                            <div class="rank-th rank-col-icon"></div>
                             <div class="rank-th rank-col-num">#</div>
                             <div class="rank-th rank-col-name">Char Name</div>
                             <div class="rank-th rank-col-job">Job</div>
@@ -37,60 +36,45 @@
                             <div class="rank-th rank-col-kill">Kill Point</div>
                         </div>
 
-                        <!-- Blade Master Row -->
-                        <div class="rank-data-row">
-                            <div class="rank-class-cell">
-                                <img src="{{ asset('images/blade-master.png') }}" alt="Blade Master"
-                                    class="rank-class-icon">
-                                <div class="rank-class-name">BLADE MASTER</div>
-                            </div>
-                            <div class="rank-rows-wrapper">
-                                <div class="rank-row-data">
-                                    <div class="rank-td rank-col-num" data-label="Rank">1</div>
-                                    <div class="rank-td rank-col-name" data-label="Character">Capeskin</div>
-                                    <div class="rank-td rank-col-job" data-label="Job">Blade master</div>
-                                    <div class="rank-td rank-col-guild" data-label="Guild">Everyone</div>
-                                    <div class="rank-td rank-col-kill" data-label="Kill Point">6</div>
-                                </div>
-                            </div>
+                        <!-- Data Rows -->
+                        <div class="rank-row-data">
+                            <div class="rank-td rank-col-num" data-label="Rank">1</div>
+                            <div class="rank-td rank-col-name" data-label="Character">Capeskin</div>
+                            <div class="rank-td rank-col-job" data-label="Job">Blade master</div>
+                            <div class="rank-td rank-col-guild" data-label="Guild">Everyone</div>
+                            <div class="rank-td rank-col-kill" data-label="Kill Point">6</div>
                         </div>
 
-                        <!-- Renegade Rows -->
-                        <div class="rank-data-row rank-multi-row">
-                            <div class="rank-class-cell">
-                                <img src="{{ asset('images/renegade.png') }}" alt="Renegade" class="rank-class-icon">
-                                <div class="rank-class-name">RENEGADE</div>
-                            </div>
-                            <div class="rank-rows-wrapper">
-                                <div class="rank-row-data">
-                                    <div class="rank-td rank-col-num">1</div>
-                                    <div class="rank-td rank-col-name">Toge</div>
-                                    <div class="rank-td rank-col-job">Renegade</div>
-                                    <div class="rank-td rank-col-guild">Underware</div>
-                                    <div class="rank-td rank-col-kill">31</div>
-                                </div>
-                                <div class="rank-row-data">
-                                    <div class="rank-td rank-col-num">2</div>
-                                    <div class="rank-td rank-col-name">Y</div>
-                                    <div class="rank-td rank-col-job">Renegade</div>
-                                    <div class="rank-td rank-col-guild">Everyone</div>
-                                    <div class="rank-td rank-col-kill">17</div>
-                                </div>
-                                <div class="rank-row-data">
-                                    <div class="rank-td rank-col-num">3</div>
-                                    <div class="rank-td rank-col-name">Anae</div>
-                                    <div class="rank-td rank-col-job">Renegade</div>
-                                    <div class="rank-td rank-col-guild">Everyone</div>
-                                    <div class="rank-td rank-col-kill">11</div>
-                                </div>
-                                <div class="rank-row-data">
-                                    <div class="rank-td rank-col-num">4</div>
-                                    <div class="rank-td rank-col-name">Cupektong</div>
-                                    <div class="rank-td rank-col-job">Renegade</div>
-                                    <div class="rank-td rank-col-guild">Underware</div>
-                                    <div class="rank-td rank-col-kill">11</div>
-                                </div>
-                            </div>
+                        <div class="rank-row-data">
+                            <div class="rank-td rank-col-num" data-label="Rank">2</div>
+                            <div class="rank-td rank-col-name" data-label="Character">Toge</div>
+                            <div class="rank-td rank-col-job" data-label="Job">Renegade</div>
+                            <div class="rank-td rank-col-guild" data-label="Guild">Underware</div>
+                            <div class="rank-td rank-col-kill" data-label="Kill Point">31</div>
+                        </div>
+
+                        <div class="rank-row-data">
+                            <div class="rank-td rank-col-num" data-label="Rank">3</div>
+                            <div class="rank-td rank-col-name" data-label="Character">Y</div>
+                            <div class="rank-td rank-col-job" data-label="Job">Renegade</div>
+                            <div class="rank-td rank-col-guild" data-label="Guild">Everyone</div>
+                            <div class="rank-td rank-col-kill" data-label="Kill Point">17</div>
+                        </div>
+
+                        <div class="rank-row-data">
+                            <div class="rank-td rank-col-num" data-label="Rank">4</div>
+                            <div class="rank-td rank-col-name" data-label="Character">Anae</div>
+                            <div class="rank-td rank-col-job" data-label="Job">Renegade</div>
+                            <div class="rank-td rank-col-guild" data-label="Guild">Everyone</div>
+                            <div class="rank-td rank-col-kill" data-label="Kill Point">11</div>
+                        </div>
+
+                        <div class="rank-row-data">
+                            <div class="rank-td rank-col-num" data-label="Rank">5</div>
+                            <div class="rank-td rank-col-name" data-label="Character">Cupektong</div>
+                            <div class="rank-td rank-col-job" data-label="Job">Renegade</div>
+                            <div class="rank-td rank-col-guild" data-label="Guild">Underware</div>
+                            <div class="rank-td rank-col-kill" data-label="Kill Point">11</div>
                         </div>
                     </div>
 
